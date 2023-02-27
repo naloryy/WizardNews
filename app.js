050cfe07f1ce5e9a310d6f0af2b48ca7619becdb
Nalory01 <Panalory@gmail.com>
2023-02-27 14:29:48 -0600

    listing posts

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const morgan = require ("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) =>{
+
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    </head>
+    <body>
+      <ul>
+        ${posts.map(post => `<li>${}`)}
+})
+
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-//
