16133c8f8cf1c7c48962f1e5c868bb0305b5f4c7
Nalory01 <Panalory@gmail.com>
2023-02-28 13:19:14 -0600

    deploy

--- app.js
+++ app.js
@@ -1,29 +1,101 @@
 const express = require("express");
-const morgan = require ("morgan");
+const morgan = require("morgan");
 const postBank = require("./postBank");
+
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(express.static("public"));
 
-app.get("/", (req, res) =>{
+app.use(morgan("dev"));
 
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else {
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <link rel="stylesheet" href="/style.css" />
+    <title>Wizard News</title>
+    </head>
+    <body>
+    <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
+        <p>
+          <span class="news-position">${post.id}. ▲</span>
+          ${post.title}
+          <small>(by ${post.name})</small>
+        </p>
+      </div>
+  </div>
+      </body>
+      </html>`;
+      res.send(html);
+  }
+});
+
+
+
+app.get("/", (req, res) => {
   const posts = postBank.list();
 
   const html = `<!DOCTYPE html>
   <html>
   <head>
+  <link rel="stylesheet" href="/style.css" />
     <title>Wizard News</title>
     </head>
     <body>
-      <ul>
-        ${posts.map(post => `<li>${}`)}
-})
+    <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+    ${posts
+      .map(
+        (post) => `
+      <div class='news-item'>
+        <p>
+          <span class="news-position">${post.id}. ▲</span>
+          <a href="/posts/${post.id}">${post.title}</a>
 
+          <small>(by ${post.name})</small>
+        </p>
+        <small class="news-info">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+      </div>`
+      )
+      .join("")}
+  </div>
+      </body>
+      </html>`;
 
+  res.send(html);
+});
 
-const PORT = 1337;
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+const path = require("path");
+app.use("/static", express.static(path.join(__dirname, "public")));
